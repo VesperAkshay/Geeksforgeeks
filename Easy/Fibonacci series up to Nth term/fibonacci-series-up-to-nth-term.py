30e80e2889d4f028f29258032575f5b58d6f3713
Akshay Patel <118452811+VesperAkshay@users.noreply.github.com>
2024-03-23 20:07:46 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Fibonacci series up to Nth term/fibonacci-series-up-to-nth-term.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def series(self, n):
+        MOD = 10**9 + 7
+        fib = [0] * (n + 1)
+        fib[0] = 0
+        if n > 0:
+            fib[1] = 1
+
+        for i in range(2, n + 1):
+            fib[i] = (fib[i - 1] + fib[i - 2]) % MOD
+
+        return fib
+        # Code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        result = ob.series(N)
+        print(*result)
+# } Driver Code Ends
